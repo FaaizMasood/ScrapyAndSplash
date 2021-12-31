8cad260b3f6f1854fd0fda6acaf75d38dc0d98aa
Faaiz <fmasood@clarku.edu>
2021-12-30 23:07:00 -0500

    First Spider Ever

--- projects/amazon/amazon/spiders/listings.py
+++ projects/amazon/amazon/spiders/listings.py
@@ -1,4 +1,5 @@
 import scrapy
+from ..items import AmazonItem
 
 
 class ListingsSpider(scrapy.Spider):
@@ -7,5 +8,21 @@ class ListingsSpider(scrapy.Spider):
 
     # return the source code of the website
     def parse(self, response):
-        title = response.css('title').extract()
-        yield {'titleText': title}
+        # title = response.css('title::text').extract()
+        # yield {'titleText': title}  # yeild is a type of return here
+
+        all_listings = response.css('div.quote')
+
+        # instance of items class
+        items = AmazonItem()
+
+        for x in all_listings:  # for loop to organise the data of all listings
+            title = x.css('span.text::text').extract()
+            author = x.css('.author::text').extract()
+            tag = x.css('.tag::text').extract()
+
+            items['title'] = title
+            items['author'] = author
+            items['tag'] = tag
+
+            yield items
